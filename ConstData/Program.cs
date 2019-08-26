c94888c4475a51a05cea9840a1a6c7296dcbb16e
Jacob Archambault <jacob.archambault@waystar.com>
2019-08-26 16:52:00 -0400

    Add PI and fixedStr consts.

--- ConstData/Program.cs
+++ ConstData/Program.cs
@@ -8,8 +8,23 @@ namespace ConstData
 {
     class Program
     {
+        class MyMathClass
+        {
+            public const double PI = 3.14;
+        }
         static void Main(string[] args)
         {
+            Console.WriteLine("***** Fun with Const *****\n");
+            Console.WriteLine("The value of PI is: {0}", MyMathClass.PI);
+            LocalConstStringVariable();
+
+            Console.ReadLine();
+        }
+
+        static void LocalConstStringVariable()
+        {
+            const string fixedStr = "Fixed string Data";
+            Console.WriteLine(fixedStr);
         }
     }
 }
